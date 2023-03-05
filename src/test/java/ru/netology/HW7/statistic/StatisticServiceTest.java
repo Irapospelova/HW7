e0d4b9bc5c85ec797e3bc7524221b78dd50a69f6
Irina Pospelova <irapospelova78@gmail.com>
2023-03-05 22:33:58 +0300

    Build sucsess

--- src/test/java/ru/netology/HW7/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/HW7/statistic/StatisticServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticServiceTest {
 
     }
 
+    @Test
+    public void findMaxIfMaxNotInTheBegining() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 32, 8, 6, 11, 11, 12};
+        long expected = 32;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
 }
